754e06cf08e5e152c7953a7c8bed4875e7767428
eduardo <dev.edusc@gmail.com>
2021-10-12 22:14:19 -0300

    Finished Navigation Graph Connections, succesfully tested first connection (title to collection).

--- app/src/main/java/com/duhdoesk/supertrunfoclone/MainActivity.kt
+++ app/src/main/java/com/duhdoesk/supertrunfoclone/MainActivity.kt
@@ -2,10 +2,18 @@ package com.duhdoesk.supertrunfoclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
--- app/src/main/java/com/duhdoesk/supertrunfoclone/title/TitleFragment.kt
+++ app/src/main/java/com/duhdoesk/supertrunfoclone/title/TitleFragment.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.duhdoesk.supertrunfoclone.R
 
 class TitleFragment : Fragment() {
-    lateinit var buttonPlay: Button
+    lateinit var buttonPlay : Button
+
     //Inflating and Returning the View with DataBindingUtil
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +20,11 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        val view = inflater.inflate(R.layout.fragment_title, container, false)
+        buttonPlay = view.findViewById(R.id.buttonPlayNow)
+        buttonPlay.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_destination_title_to_destination_collection, null))
+        return view
     }
+
 }
