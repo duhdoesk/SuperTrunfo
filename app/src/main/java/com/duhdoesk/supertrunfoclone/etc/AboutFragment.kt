896b408ead4c784dd132226d39146c03bc1aee1b
eduardo <dev.edusc@gmail.com>
2021-10-17 12:12:54 -0300

    created about page using https://github.com/medyo/android-about-page

--- app/src/main/java/com/duhdoesk/supertrunfoclone/etc/AboutFragment.kt
+++ app/src/main/java/com/duhdoesk/supertrunfoclone/etc/AboutFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.duhdoesk.supertrunfoclone.R
+import mehdi.sakout.aboutpage.AboutPage
+import java.util.*
 
 class AboutFragment : Fragment() {
     //Inflating and Returning the View with DataBindingUtil
@@ -14,7 +16,20 @@ class AboutFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        var aboutPage = AboutPage(context)
+            .isRTL(false)
+            .setImage(R.drawable.about_header)
+            .setDescription(getString(R.string.about_description))
+            .addGroup("Other Projects")
+            .addGitHub("duhdoesk", "GitHub")
+            .addGroup("Contact me")
+            .addEmail("dev.edusc@gmail.com", "E-mail")
+            .addInstagram("duscaranari", "Instagram")
+            .addFacebook("du.scaranari", "Facebook")
+            .addWebsite("https://wa.me/5511994760191", "WhatsApp")
+
+            .create()
+
+        return aboutPage
     }
 }
