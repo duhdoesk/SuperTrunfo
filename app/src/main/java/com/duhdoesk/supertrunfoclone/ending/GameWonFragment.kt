5ed49b5b83565c5f9f7247c71772cba3335a4d25
edusc <dev.edusc@gmail.com>
2022-03-15 21:58:12 -0300

    designed game won/over layouts, also finished all the navigating stuff. game is now fully playable with the big amount of 4 total cards

--- app/src/main/java/com/duhdoesk/supertrunfoclone/ending/GameWonFragment.kt
+++ app/src/main/java/com/duhdoesk/supertrunfoclone/ending/GameWonFragment.kt
@@ -5,16 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.duhdoesk.supertrunfoclone.R
+import com.duhdoesk.supertrunfoclone.databinding.FragmentGameOverBinding
+import com.duhdoesk.supertrunfoclone.databinding.FragmentGameWonBinding
 
 class GameWonFragment : Fragment() {
+
+    private var _binding: FragmentGameWonBinding? = null
+    private val binding get() = _binding!!
+
     //Inflating and Returning the View with DataBindingUtil
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_won, container, false)
+        _binding = FragmentGameWonBinding.inflate(inflater, container, false)
+        return binding.apply {
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btLetsDoIt: Button = binding.btLetsDoIt
+        val btNotNow: Button = binding.btNotNow
+
+        btLetsDoIt.setOnClickListener(View.OnClickListener {
+            findNavController().navigate(GameWonFragmentDirections.actionDestinationGameWonToDestinationCollection())
+        })
+
+        btNotNow.setOnClickListener(View.OnClickListener {
+            findNavController().navigate(GameWonFragmentDirections.actionDestinationGameWonToDestinationTitle())
+        })
     }
 }
